9f86df5acfed49822b3b3b357e12bd80fc565eff
Emerson Dias de Oliveira <emersondiaspd@gmail.com>
2022-09-10 23:47:36 -0300

    Ajustes Ranking/Sobre

--- app/src/main/java/br/com/marco/jokenpo/RankingActivity.kt
+++ app/src/main/java/br/com/marco/jokenpo/RankingActivity.kt
@@ -1,56 +1,49 @@
 package br.com.marco.jokenpo
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.R
+import android.content.ContentValues.TAG
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import br.com.marco.jokenpo.databinding.ActivityGameOverBinding
+import android.util.Log
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
 import br.com.marco.jokenpo.databinding.ActivityRankingBinding
 import br.com.marco.jokenpo.firestore.Ranking
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 
 class RankingActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRankingBinding
-    lateinit var recyclerView: RecyclerView
+    private val listRanking = ArrayList<String>()
+    private val database = FirebaseFirestore.getInstance()
 
-    lateinit var ref:DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityRankingBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val score = intent.getIntExtra("score",0);
-        recuperaFirestore()
 
+        this.listRanking.add("Aguarde...")
+        binding.listview.adapter = ArrayAdapter<String>(this, R.layout.simple_list_item_1, listRanking)
 
+        this.recuperaFirestore()
     }
 
     fun recuperaFirestore() {
-        var mAuth: FirebaseAuth = FirebaseAuth.getInstance()
-        val db = FirebaseFirestore.getInstance()
-        val listRanking =  arrayListOf<Ranking>()
-        val collection =
-            db.collection("ranking").get().addOnSuccessListener { result ->
-                try {
-                    for  (document in result ){
-                        listRanking.add(document.toObject(Ranking::class.java));
-                    }
+        this.listRanking.clear()
 
-                } finally {
-                    var recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-                    val adapter = CustomAdapter(this, listRanking)
-                    recyclerView.layoutManager = LinearLayoutManager(this)
-                    recyclerView.adapter = adapter
-
+        // TODO: Como a coluna score é string, verificar como fazer, se altera, ou se tem como ordenar como int
+        // TODO: E ordenar por score e nome
+        database.collection("ranking")
+            .orderBy("score", Query.Direction.DESCENDING).get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    this.listRanking.add(document.data.get("nome").toString() + ": " + document.data.get("score").toString())
                 }
-            }
-        var recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-        val adapter = CustomAdapter(this, listRanking)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = adapter
-    }
 
+                binding.listview.adapter = ArrayAdapter<String>(this, R.layout.simple_list_item_1, this.listRanking)
+            }
+            .addOnFailureListener { exception  ->
+                Log.w(TAG, "Error getting documents : $exception")
+            }
+    }
 }
